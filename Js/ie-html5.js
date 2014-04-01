5c3bcdc2adde8ffafce4e0416c00b8699b4daa5b
XeonWell <admin@xw.hb.cn>
2014-04-01 10:45:34 +0800

    remove js dir

--- Js/ie-html5.js
+++
@@ -1,5 +0,0 @@
-//create html5 element for IE
-(function(){
-var h5e = "address|article|aside|audio|canvas|command|datalist|details|dialog|figure|figcaption|footer|header|hgroup|keygen|mark|meter|menu|nav|progress|ruby|section|time|video".split('|');
-for(var i = 0, j = h5e.length; i < j; i++) document.createElement(h5e[i]);
-})();
--- Js/stat.js
+++
@@ -1,3 +0,0 @@
-
-var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_2098087'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s11.cnzz.com/stat.php%3Fid%3D2098087' type='text/javascript'%3E%3C/script%3E"));
