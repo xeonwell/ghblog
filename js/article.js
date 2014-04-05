cbebe16876710dfa40075077c400ed76178595ae
xeonwell <admin@xw.hb.cn>
2014-04-05 22:59:57 +0800

    update site

--- js/article.js
+++ js/article.js
@@ -1,11 +1,10 @@
 /**
- * Author: admin@xw81.com
+ * Author:  admin@xw.hb.cn
  */
 $(function(){
 	if ($("pre[class^='brush']").size() > 0){
 		$.getScript("/js/syntaxhighlighter/shCore.js",function(){
 			$.getScript("/js/syntaxhighlighter/shBrush.js",function(){
-				SyntaxHighlighter.config.clipboardSwf = '/js/syntaxhighlighter/clipboard.swf';
 				SyntaxHighlighter.config.bloggerMode = true;
 				SyntaxHighlighter.highlight();
 			});
--- js/ie-fix.js
+++ js/ie-fix.js
@@ -1,7 +1,3 @@
 $(function(){
 	document.execCommand('BackgroundImageCache', false, true);//open background-image caching for IE6
-	if($(document.body).width()<850) $("#searchDiv").fadeOut("slow");
-	$(window).resize(function(){
-		$("#searchDiv").toggle($(document.body).width()>850);
-	});
 });
